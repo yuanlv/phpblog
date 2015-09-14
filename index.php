b5ddcd020557a0bf9907cc5b296850ccfcf99ccb
yuanlv <yuanlv@126.com>
2015-09-14 22:07:59 +0800

    weixin test

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
-<?php include_once "backend.php"; ?>
-<html>
-<head>
-</head>
-<body>
-<h1><?php the_title(); ?></h1>
+<?php
+	function weixin_jieru(){
+		echo $_GET["echostr"];
+	}
 
-
-
-
-<h3>Meta</h3>
-<?php if (!isset($_COOKIE['username'])): ?>
-	<form action="login.php" method="post">
-		<label for="user">User:</label> <input type="text" name="user"></input><br/>
-		<label for="user">Password:</label> <input type="password" name="password"></input><br/>
-		<input type="submit"/>
-	</form>
-<?php else : ?>
-	
-	<p>Logged in as <?php echo $_COOKIE['username']."." ?> <a href="admin/index.php">Go to admin</a>
-	</p>
-<?php endif ?>
-
-</body>
+	echo weixin_jieru();
+?>
