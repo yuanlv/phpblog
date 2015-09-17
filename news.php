cd4f6e14489d1cafc693ce4a50d20e055ab00ca7
yuanlv <yuanlv@126.com>
2015-09-17 21:18:27 +0800

    weixin response news

--- news.php
+++ news.php
@@ -4,7 +4,7 @@
  * 从百度新闻中查询关键字，显示搜索结果
  */
 function getNews($keyword){
-	$url = "http://news.baidu.com/ns?word=".$keywork."&tn=news&from=news&cl=2&rn=20&ct=1&oq=yuer&f=3&rsp=1";
+	$url = "http://news.baidu.com/ns?word=".$keyword."&tn=news&from=news&cl=2&rn=20&ct=1&oq=yuer&f=3&rsp=1";
 	print_r($url);
 	$curl = curl_init($url);
 	$news = curl_exec($curl);
