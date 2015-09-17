d59c6e75d39ea569970d30efc3c60c77cff2f92e
yuanlv <yuanlv@126.com>
2015-09-17 20:55:03 +0800

    weixin response news

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  */
 function getNews($keyword){
 	$url = "http://news.baidu.com/ns?word=".$keywork."&tn=news&from=news&cl=2&rn=20&ct=1&oq=yuer&f=3&rsp=1";
-	print_r($url)
+	print_r($url);
 	$curl = curl_init($url);
 	$news = curl_exec($curl);
 	curl_close($curl);
