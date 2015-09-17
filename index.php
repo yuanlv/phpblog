3b89055f68c930ec012558ee3a94a7cabe636094
yuanlv <yuanlv@126.com>
2015-09-17 20:39:42 +0800

    weixin response news

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
 /**
- * 从百度新闻中查询关键字，显示搜索结果
- */
-function getNews($keyword){
-	$url = "http://news.baidu.com/ns?word=".$keywork."&tn=news&from=news&cl=2&rn=20&ct=1&oq=yuer&f=3&rsp=1";
-	print_r($url)
-	$curl = curl_init($url);
-	$news = curl_exec($curl);
-	curl_close($curl);
-
-	return $news[0];
-}
-echo "getnews...<br/>";
-echo getNews("育儿");
-
-/**
   * wechat php test
   */
 
